c6fafaf02d655f8e29f26da37a633f07ebdadf92
tktk981377-png <tktk981377@gmail.com>
2026-02-23 21:34:26 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const line = require("@line/bot-sdk");
+
+const app = express();
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+app.post("/webhook", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  const client = new line.Client(config);
+
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: "Bot動いてるよ🔥",
+  });
+}
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
