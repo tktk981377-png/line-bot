b07cc9140a64c7210b9a0f3b4934eb4e77410088
tktk981377-png <tktk981377@gmail.com>
2026-02-23 22:04:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ function handleEvent(event) {
   });
 }
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
